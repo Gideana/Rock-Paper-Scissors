0e94136c10e8c1ae863529b569e11ffa977d2aa6
Gideana <celina.mayrhuber@gmail.com>
2022-11-04 11:27:58 +0000

    Fix play 5 rounds function

--- main.js
+++ main.js
@@ -18,6 +18,9 @@ let computerScore = 0;
 let numberOfRounds = 0;
 
 const rounds = document.createElement('p');
+const tie = document.createElement('div');
+const win = document.createElement('div');
+const loss = document.createElement('div');
 
 const buttons = document.querySelectorAll('button');
     buttons.forEach((button) => {
@@ -27,43 +30,42 @@ const buttons = document.querySelectorAll('button');
             rounds.innerHTML='';
                 function playSingleRound() {
                     let computerSelection = getComputerChoice();
-                    if ((numberOfRounds < 5) &&
-                    (playerSelection === computerSelection)) {
+                    if (playerSelection === computerSelection) {
                         playerScore ++;
                         computerScore ++;
                         numberOfRounds ++;
-                        const tie = document.createElement('div');
                         tie.textContent = 'It\' a tie! ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
                         results.appendChild(tie); 
                         rounds.textContent = 'You have played ' + numberOfRounds + ' rounds.'
                         Number_Of_Rounds.appendChild(rounds);
-                    } else if ((numberOfRounds < 5) && 
-                    ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+                        if (numberOfRounds === 5) {
+                            return(playGame());
+                        }
+                    } else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
                     (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
-                    (playerSelection === 'Paper' && computerSelection === 'Rock'))) {
+                    (playerSelection === 'Paper' && computerSelection === 'Rock')) {
                         playerScore ++;
                          numberOfRounds ++;
-                        const win = document.createElement('div');
                         win.textContent = 'You win! ' + playerSelection + ' beats ' + computerSelection + '! Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
                         results.appendChild(win);
                         rounds.textContent = 'You have played ' + numberOfRounds + ' rounds.'
                         Number_Of_Rounds.appendChild(rounds);
-                    } else if ((numberOfRounds < 5) &&
-                    ((playerSelection === 'Scissors' && computerSelection === 'Rock') ||
+                        if (numberOfRounds === 5) {
+                            return(playGame());
+                        }
+                    } else if ((playerSelection === 'Scissors' && computerSelection === 'Rock') ||
                     (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
-                    (playerSelection === 'Rock' && computerSelection === 'Paper'))) {
+                    (playerSelection === 'Rock' && computerSelection === 'Paper')) {
                         computerScore ++;
                         numberOfRounds ++;
-                        const loss = document.createElement('div');
                         loss.textContent = 'You lose! ' + computerSelection + ' beats ' + playerSelection + '! Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
                         results.appendChild(loss);
                         rounds.textContent = 'You have played ' + numberOfRounds + ' rounds.'
                         Number_Of_Rounds.appendChild(rounds);
-                    } else if (numberOfRounds === 5) {
-                        //at the moment you have to click one more time to see end results, needs to be fixed
-                        //maybe change the above function instead of having it as first condition put it in after as new if each time?
-                        return(playGame())
-                    }
+                        if (numberOfRounds === 5) {
+                            return(playGame());
+                        }
+                    } 
                 }
                 return(playSingleRound())
 
